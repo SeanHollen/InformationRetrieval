1c53ac5ea9dc61e57544714b6b42c380e12046b9
sean.hollen <hollen.s@husky.neu.edu>
2020-11-02 06:57:35 -0500

    new code added

--- IRProject/src/main/java/HW2/Tokenizer.java
+++ IRProject/src/main/java/HW2/Tokenizer.java
@@ -3,32 +3,37 @@ package HW2;
 import org.javatuples.Triplet;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class Tokenizer {
 
+  // used
   private HashMap<String, String> wordSubstitutions;
   private HashSet<String> stopwords;
+  private HashMap<String, String> documents;
+  // mappings
   private HashMap<Integer, String> docHashes;
   private HashMap<Integer, String> tokens;
-  private HashMap<Integer, Integer> vocabSize;
+  // derived
   private int numTokens;
-  private double avgNumTokens;
-  private HashMap<String, String> documents;
+  private double avgDocLength;
+  private HashMap<Integer, Integer> docLengthsMap;
 
   public Tokenizer() {
     // Token Hash -> Token
     tokens = new HashMap<Integer, String>();
-    // Token Hash -> count
-    vocabSize = new HashMap<Integer, Integer>();
     // Document Hash -> Document
     docHashes = new HashMap<Integer, String>();
     // Document ID -> Document text
     documents = new HashMap<String, String>();
+    // Document Hash -> Document Length
+    docLengthsMap = new HashMap<Integer, Integer>();
   }
 
   public Tokenizer(HashSet<String> stopwords, HashMap<String, String> wordSubstitutions) {
@@ -49,43 +54,85 @@ public class Tokenizer {
   }
 
   public void index(String outDir) {
-    int count = 0;
-    for (String docId : documents.keySet()) {
-      count++;
-      int docHash = docId.hashCode();
-      ArrayList<Triplet> tokens = tokenize(documents.get(docId), docHash, true);
-      numTokens += tokens.size();
-      vocabSize.put(docHash, tokens.size());
+    String[] docKeys = (String[]) documents.keySet().toArray();
+    for (int i = 1; 1000 < documents.size() / i; i++) {
+      for (String docKey : docKeys) {
+        int docHash = docKey.hashCode();
+        ArrayList<Triplet> tokens = tokenize(documents.get(docKey), docHash, true);
+        numTokens += tokens.size();
+        docLengthsMap.put(docHash, tokens.size());
+      }
       try {
-        String catPath = outDir + "/catalogs/" + count + ".txt";
+        String catPath = outDir + "catalogs/" + i + ".txt";
+        String invPath = outDir + "invList/" + i + ".txt";
         File catalog = new File(catPath);
-        if (catalog.createNewFile()) {
-          System.out.println("File created: " + catalog.getName());
+        File invIndex = new File(invPath);
+        if (catalog.createNewFile() && invIndex.createNewFile()) {
+          System.out.println("2 files created");
         } else {
-          System.out.println("Not created");
+          System.out.println("At least one file not created (perhaps already exists)");
         }
         FileWriter catWriter = new FileWriter(catPath);
-        String invPath = outDir + "/invList/" + count + ".txt";
-        File invIndex = new File(invPath);
-        if (invIndex.createNewFile()) {
-          System.out.println("File created: " + invIndex.getName());
-        } else {
-          System.out.println("Not created");
-        }
         FileWriter indexWriter = new FileWriter(invPath);
+
+        // todo 
         catWriter.write("");
         indexWriter.write("");
+
         catWriter.close();
         indexWriter.close();
       } catch (IOException e) {
         e.printStackTrace();
       }
     }
-    avgNumTokens = (double) numTokens / (double) docHashes.size();
+    avgDocLength = (double) numTokens / (double) docHashes.size();
+    System.out.println("Vocabulary size: " + tokens.size());
+    System.out.println("Number of docs: " + documents.size());
+    System.out.println("Aggregate number of Tokens: " + numTokens);
+    System.out.println("Average Number of Tokens: " + avgDocLength);
+    putToFile(outDir + "docIds.txt", docHashes);
+    putToFile(outDir + "tokenIds.txt", tokens);
+    putToFile(outDir + "docLengths.txt", docLengthsMap);
+    putToFile(outDir + "aggInfo.txt", new Integer[]
+            {tokens.size(), documents.size(), numTokens, (int) avgDocLength});
   }
 
-  public void merge() {
+  private void putToFile(String location, Object toPut) {
+    try {
+      File file = new File(location);
+      if (file.createNewFile()) {
+        System.out.println("created a file");
+      } else {
+        System.out.println("failed to create a file");
+      }
+      ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+      oos.writeObject(toPut);
+      oos.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
+  public void merge(String dir) {
+    File[] catalogs = new File(dir + "/catalogs").listFiles();
+    File[] invLists = new File(dir + "/invList").listFiles();
+    if (catalogs == null || invLists == null) {
+      throw new IllegalArgumentException("failure to find files in dir");
+    }
+    while (catalogs.length > 1 && invLists.length > 1) {
+      for (int i = 0; i < catalogs.length; i += 2) {
+        String cat1Name = catalogs[i].getName();
+        String cat2Name = catalogs[i+1].getName();
+        String inv1Lists = invLists[i].getName();
+        String inv2Lists = invLists[i+1].getName();
+        //todo
+      }
+    }
+    catalogs = new File(dir + "/catalogs").listFiles();
+    invLists = new File(dir + "/invList").listFiles();
+    if (catalogs == null || invLists == null) {
+      throw new IllegalArgumentException("failure to find files in dir");
+    }
   }
 
   public ArrayList<Triplet> tokenize(String document, int from, boolean doStemming) {
@@ -116,12 +163,12 @@ public class Tokenizer {
     return tokens.get(hash);
   }
 
-  public int vocabSizeOf(int docHash) {
-    return vocabSize.get(docHash);
+  public double getAvgNumTokens() {
+    return avgDocLength;
   }
 
-  public double getAvgNumTokens() {
-    return avgNumTokens;
+  public int docLength(int docHash) {
+    return docLengthsMap.get(docHash);
   }
 
 }
