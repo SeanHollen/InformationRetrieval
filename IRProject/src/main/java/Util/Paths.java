254cbc343175074029cba2a0aee3eec4598ae998
sean.hollen <hollen.s@husky.neu.edu>
2020-12-29 02:25:21 -0500

    paths bug

--- IRProject/src/main/java/Util/Paths.java
+++ IRProject/src/main/java/Util/Paths.java
@@ -4,7 +4,7 @@ public class Paths {
 
   private static final String prefix = "IRProject/";
 
-  public static final String stoplist = prefix + "/Users/sean.hollen/Downloads/elasticsearch-7.9.1/config/stoplist.txt";
+  public static final String stoplist = "/Users/sean.hollen/Downloads/elasticsearch-7.9.1/config/stoplist.txt";
   public static final String queryFile = prefix + "IR_Data/AP_DATA/queries_v4.txt";
   public static final String docsToParse = prefix + "IR_Data/AP_DATA/ap89_collection";
   public static final String sitesToParse = prefix + "out/CrawledDocuments";
